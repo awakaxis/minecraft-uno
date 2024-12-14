0bf3006afe319fc6f8509c33d8945934c07e85b8
awakaxis <ashton5350@gmail.com>
2024-12-14 03:45:18 -0800

    playing decks can be created naturally now by using a card item on a top facing colliding block face

--- src/main/java/net/awakaxis/uno/item/UnoCardItem.java
+++ src/main/java/net/awakaxis/uno/item/UnoCardItem.java
@@ -1,7 +1,23 @@
 package net.awakaxis.uno.item;
 
+import net.awakaxis.uno.UNO;
+import net.awakaxis.uno.UNOEntities;
+import net.awakaxis.uno.entity.PlayingDeck;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.IntTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 
 public class UnoCardItem extends Item {
 
@@ -18,6 +34,36 @@ public class UnoCardItem extends Item {
         ItemStack itemStack = getDefaultInstance();
         itemStack.getOrCreateTag().putInt(CARD_INDEX_TAG, index);
         return itemStack;
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext useOnContext) {
+        Level level = useOnContext.getLevel();
+        if (!(level instanceof ServerLevel serverLevel)) {
+            return InteractionResult.PASS;
+        } else {
+            BlockPos hitPos = useOnContext.getClickedPos();
+            BlockState state = serverLevel.getBlockState(hitPos);
+
+            if (useOnContext.getClickedFace() != Direction.UP || state.getCollisionShape(serverLevel, hitPos).isEmpty()) return InteractionResult.SUCCESS;
+
+            ItemStack itemStack = useOnContext.getItemInHand();
+            Vec3 hitLocation = useOnContext.getClickLocation();
+
+            EntityType<PlayingDeck> entityType = UNOEntities.PLAYING_DECK_ENTITY_TYPE;
+            CompoundTag compoundTag = new CompoundTag();
+            compoundTag.putString("id", EntityType.getKey(entityType).toString());
+
+            // why is creating entities so cancer, this shit took like an hour to figure out
+            PlayingDeck playingDeck = (PlayingDeck) EntityType.create(compoundTag, serverLevel).orElseThrow();
+            playingDeck.moveTo(hitLocation);
+            playingDeck.pushCard(useOnContext.getPlayer(), itemStack.getOrCreateTag().getInt(CARD_INDEX_TAG));
+            serverLevel.addFreshEntityWithPassengers(playingDeck);
+
+            itemStack.shrink(1);
+
+            return InteractionResult.CONSUME;
+        }
     }
 
     public UnoCardItem(Item.Properties properties) {

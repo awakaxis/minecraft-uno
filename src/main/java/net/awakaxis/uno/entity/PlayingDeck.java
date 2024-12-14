7c2eed2e59093304362dd2cf5e81020612ec0e62
awakaxis <ashton5350@gmail.com>
2024-12-14 03:43:36 -0800

    a rotation value is stored and synced now too, so that the direction the player is facing when placing the card serves as the origin on which the minor random rotations are applied

--- src/main/java/net/awakaxis/uno/entity/PlayingDeck.java
+++ src/main/java/net/awakaxis/uno/entity/PlayingDeck.java
@@ -1,6 +1,8 @@
 package net.awakaxis.uno.entity;
 
 import net.awakaxis.uno.UNO;
+import net.awakaxis.uno.UNOItems;
+import net.awakaxis.uno.item.UnoCardItem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.nbt.*;
@@ -13,6 +15,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -22,21 +25,25 @@ public class PlayingDeck extends Entity {
     private static final RandomSource RANDOM = RandomSource.create();
     public static final EntityDataAccessor<CompoundTag> DECK_CONTENTS_ID = SynchedEntityData.defineId(PlayingDeck.class, EntityDataSerializers.COMPOUND_TAG);
     public static final EntityDataAccessor<Long> CARD_PLACEMENT_SEED = SynchedEntityData.defineId(PlayingDeck.class, EntityDataSerializers.LONG);
+    public static final String DECK_ROTS_TAG = "cardRotations";
     public static final String DECK_STACK_TAG = "cardStack";
 
     public PlayingDeck(EntityType<?> entityType, Level level) {
         super(entityType, level);
-
     }
 
-    public void pushCard(int cardIndex) {
-        if (this.level().isClientSide) return;
-
-        UNO.LOGGER.info("pushing card");
+    public void pushCard(Player player, int cardIndex) {
+        if (this.level().isClientSide) {
+            UNO.LOGGER.warn("Pushing card from client");
+            return;
+        }
 
         CompoundTag deckContents = this.entityData.get(DECK_CONTENTS_ID);
         ListTag cardStack = deckContents.getList(DECK_STACK_TAG, Tag.TAG_INT);
+        ListTag cardRots = deckContents.getList(DECK_ROTS_TAG, Tag.TAG_INT);
+
         cardStack.add(IntTag.valueOf(cardIndex));
+        cardRots.add(IntTag.valueOf((int) player.getYRot()));
 
         this.entityData.set(DECK_CONTENTS_ID, deckContents, true);
     }
@@ -44,15 +51,20 @@ public class PlayingDeck extends Entity {
     public void popCard() {
         if (this.level().isClientSide) return;
 
-        UNO.LOGGER.info("popping card");
-
         CompoundTag deckContents = this.entityData.get(DECK_CONTENTS_ID);
         ListTag cardStack = deckContents.getList(DECK_STACK_TAG, Tag.TAG_INT);
-        if (cardStack.isEmpty()) return;
+        ListTag cardRots = deckContents.getList(DECK_ROTS_TAG, Tag.TAG_INT);
+        if (cardStack.isEmpty() || cardRots.isEmpty()) {
+            if (!(cardStack.isEmpty() && cardRots.isEmpty())) {
+                UNO.LOGGER.warn("Desync in cardStack and cardRots");
+            }
+            return;
+        }
 
         cardStack.remove(cardStack.size() - 1);
+        cardRots.remove(cardRots.size() - 1);
 
-        this.entityData.set(DECK_CONTENTS_ID, deckContents);
+        this.entityData.set(DECK_CONTENTS_ID, deckContents, true);
     }
 
     @Override
@@ -71,6 +83,11 @@ public class PlayingDeck extends Entity {
         }
     }
 
+//    @Override
+//    public boolean shouldRender(double d, double e, double f) {
+//        return true;
+//    }
+
     @Override
     public boolean isPickable() {
         return true;
@@ -86,11 +103,14 @@ public class PlayingDeck extends Entity {
     public void readAdditionalSaveData(CompoundTag compoundTag) {
         CompoundTag compoundTag2 = new CompoundTag();
         compoundTag2.put(DECK_STACK_TAG, compoundTag.getList(DECK_STACK_TAG, Tag.TAG_INT));
+        compoundTag2.put(DECK_ROTS_TAG, compoundTag.getList(DECK_ROTS_TAG, Tag.TAG_INT));
         this.entityData.set(DECK_CONTENTS_ID, compoundTag2);
     }
 
     @Override
     public void addAdditionalSaveData(CompoundTag compoundTag) {
-        compoundTag.put(DECK_STACK_TAG, this.entityData.get(DECK_CONTENTS_ID).getList(DECK_STACK_TAG, Tag.TAG_INT));
+        CompoundTag deckData = this.entityData.get(DECK_CONTENTS_ID);
+        compoundTag.put(DECK_STACK_TAG, deckData.getList(DECK_STACK_TAG, Tag.TAG_INT));
+        compoundTag.put(DECK_ROTS_TAG, deckData.getList(DECK_ROTS_TAG, Tag.TAG_INT));
     }
 }

3b84c97e293f73927f367fd82a4c1d2f4c4256b9
awakaxis <awakaxis@gmail.com>
2025-11-06 18:16:28 -0800

    chore: formatter

--- src/main/java/net/awakaxis/uno/client/renderer/blockentity/CardDeckRenderer.java
+++ src/main/java/net/awakaxis/uno/client/renderer/blockentity/CardDeckRenderer.java
@@ -21,18 +21,23 @@ public class CardDeckRenderer implements BlockEntityRenderer<CardDeckBlockEntity
     private static final float CARD_THICKNESS = 0.007f;
     private static final float CARD_FACTOR = 5.0f;
 
-    public CardDeckRenderer(BlockEntityRendererProvider.Context context) {}
+    public CardDeckRenderer(BlockEntityRendererProvider.Context context) {
+    }
 
     @Override
-    public void render(CardDeckBlockEntity blockEntity, float f, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j) {
-        if (blockEntity.getCardCount() <= 0) return;
+    public void render(CardDeckBlockEntity blockEntity, float f, PoseStack poseStack,
+            MultiBufferSource multiBufferSource, int i, int j) {
+        if (blockEntity.getCardCount() <= 0)
+            return;
 
         float height = CARD_THICKNESS * Math.max(Math.round((blockEntity.getCardCount() / CARD_FACTOR)) / 2.0f, 0.5f);
 
         VertexConsumer vertexConsumer = multiBufferSource.getBuffer(RenderType.entityCutout(CARD_DECK_TEXTURE));
         poseStack.pushPose();
 
-        // TODO someday: translate with blockstate (i wanna make it so you can place the deck on the left or right side of the block face, rather than just the middle)
+        // TODO someday: translate with blockstate (i wanna make it so you can place the
+        // deck on the left or right side of the block face, rather than just the
+        // middle)
         switch (blockEntity.getBlockState().getValue(CardDeckBlock.FACING)) {
             case EAST -> poseStack.rotateAround(Axis.YN.rotationDegrees(90f), 0.5f, 0, 0.5f);
             case SOUTH -> poseStack.rotateAround(Axis.YN.rotationDegrees(180f), 0.5f, 0, 0.5f);
@@ -41,7 +46,8 @@ public class CardDeckRenderer implements BlockEntityRenderer<CardDeckBlockEntity
         Matrix4f affine = poseStack.last().pose();
         Matrix3f normal = poseStack.last().normal();
 
-        //TODO: replace hardcoded vertex coordinates with variables so that implementing blockstates is easier
+        // TODO: replace hardcoded vertex coordinates with variables so that
+        // implementing blockstates is easier
 
         // top face
         this.vertex(affine, normal, vertexConsumer, 0.375f, height, 0.312f, 0, 0, 0, 1, 0, i);
@@ -51,8 +57,8 @@ public class CardDeckRenderer implements BlockEntityRenderer<CardDeckBlockEntity
 
         vertexConsumer = multiBufferSource.getBuffer(RenderType.entityCutout(CARD_DECK_TEXTURE_SIDE));
 
-        // round to x, x + 0.5, or x + 1 because the card side texture is actually two cards on top of each other, so the value needs to be in terms of 0.5
-        // hawk tuah
+        // round to x, x + 0.5, or x + 1 because the card side texture is actually two
+        // cards on top of each other, so the value needs to be in terms of 0.5
         final float VALUE = Math.max(Math.round(blockEntity.getCardCount() / CARD_FACTOR) / 2.0f, 0.5f);
         final float UV_NY = (int) VALUE < VALUE ? 0.5f : 0.0F;
         final float UV_PY = (int) VALUE < VALUE ? VALUE + 0.5f : VALUE;
@@ -85,13 +91,14 @@ public class CardDeckRenderer implements BlockEntityRenderer<CardDeckBlockEntity
         poseStack.popPose();
     }
 
-    public void vertex(Matrix4f matrix4f, Matrix3f matrix3f, VertexConsumer vertexConsumer, float i, float j, float k, float f, float g, int l, int m, int n, int o) {
+    public void vertex(Matrix4f matrix4f, Matrix3f matrix3f, VertexConsumer vertexConsumer, float i, float j, float k,
+            float f, float g, int l, int m, int n, int o) {
         vertexConsumer.vertex(matrix4f, i, j, k)
                 .color(255, 255, 255, 255)
                 .uv(f, g)
                 .overlayCoords(OverlayTexture.NO_OVERLAY)
                 .uv2(o)
-                .normal(matrix3f, (float)l, (float)m, (float)n)
+                .normal(matrix3f, (float) l, (float) m, (float) n)
                 .endVertex();
     }
 }
